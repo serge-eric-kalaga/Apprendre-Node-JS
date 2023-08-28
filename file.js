a4ce30e633b6091c4835d610d4c4b8ca85f9b622
serge-eric-kalaga <kalagaserge4@gmail.com>
2023-08-28 13:32:44 +0000

    lire le contenu d'un fichier

--- file.js
+++ file.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 
-const filename = "MonFichier.txt"
+const filename = "./MonFichier.txt"
 const filetext = "Hello world !"
 
 // creer un fichier
 fs.writeFile(filename, filetext, (err)=>{
     console.log(err);
+})
+
+// lire le contenu d'un fichier
+fs.readFile(filename, (err, data)=>{
+    if (err) {
+        console.log(err);
+    }
+    else{
+        console.log(data.toString());
+    }
 })
