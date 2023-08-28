2ec895b431f22a50a832a656b90d8f21666bcf13
serge-eric-kalaga <kalagaserge4@gmail.com>
2023-08-28 16:42:13 +0000

    redirection

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ app = express()
   
 // /home
 app.get("/home", (req, res)=>{
-    res.sendFile("./index.html", {root:__dirname});
+    res.status(200).sendFile("./index.html", {root:__dirname});
+})
+app.get("/", (req, res)=>{
+    res.status(300).redirect("/home");
 })
 
 app.use((req, res)=>{
-    res.sendFile("./404.html", {root:__dirname});
+    res.status(404).sendFile("./404.html", {root:__dirname});
 })
 
 app.listen("8080", ()=>{
