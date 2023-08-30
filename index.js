7d0a487d47b4d2afdc870df1c9770b40489d8410
serge-eric-kalaga <kalagaserge4@gmail.com>
2023-08-30 19:10:25 +0000

    simple api task list

--- index.js
+++ index.js
@@ -3,31 +3,35 @@ const morgan = require('morgan')
 
 app = express()
 
-// --------------- Definition du moteur de template --------------
-app.set("view engine", "ejs")
-app.set("views", __dirname + "/html")
-
+const task_list = [
+    {
+        id: 1, nom: "task 1", description: "description task 1", etat: true
+    },
+    {
+        id :2 ,nom:"task 2" ,description :"description task 2 ",etat:false
+    },
+    {
+        id :3 ,nom:"task 3" ,description :"description task 3 ",etat:true
+    },
+    {
+        id :4 ,nom:"task 4" ,description :"description task 4 ",etat:false
+    }
+]
 
 // --------------- Middleware --------------
 
-//static files folder
-app.use(express.static("static"))
 // Morgan for request time
 app.use(morgan("dev"))
 
 // --------------- End Middleware --------------
   
-// /home
-app.get("/home", (req, res)=>{
-    const heure = Date().toString()
-    res.status(200).render("index", {heure});
-})
+// task list
 app.get("/", (req, res)=>{
-    res.status(300).redirect("/home");
+    res.status(300).send(task_list)
 })
 
 app.use((req, res)=>{
-    res.status(404).render("404");
+    res.status(404).send("Page Not Found");
 })
 
 app.listen("8080", ()=>{
